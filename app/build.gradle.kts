56c0f2f5fba841fea17f963965d14dc4b8d420ca
Zireha <cheva.wahyu@gmail.com>
2023-12-13 06:48:01 +0700

    updating activitydetail.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,6 @@ android {
 dependencies {
 
     //material design
-    implementation("com.google.android.material:material:1.10.0")
-
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
